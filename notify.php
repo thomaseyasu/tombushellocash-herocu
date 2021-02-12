6cba27bb0a5e20b8a633f05b2e1f540ff982b59e
thomas-eyasu <eyasu.thomas@gmail.com>
2021-02-12 21:04:22 +0300

    initial commit

--- notify.php
+++ notify.php
@@ -158,8 +158,8 @@
                                                                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                                                                 CURLOPT_CUSTOMREQUEST => 'POST',
                                                                 CURLOPT_POSTFIELDS =>'{
-                                                                    "from": "$phone",
-                                                                    "amount": $price,
+                                                                    "from": "?><?php $phone;?><?php",
+                                                                    "amount": 10,
                                                                     "description": "Payment for thom bus transportation ticket",
                                                                     "currency": "ETB",
                                                                     "expires": "2021-03-03T14:47:23.000Z",
